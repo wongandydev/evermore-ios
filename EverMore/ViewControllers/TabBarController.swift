e061d8d4d9da836355ddaab2db6288cdc705b5fd
wongandydev <wongandy97@gmail.com>
2020-05-09 12:33:29 -0400

    Add tabBar controller

---
+++ EverMore/ViewControllers/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.swift
+//  EverMore
+//
+//  Created by Andy Wong on 5/9/20.
+//  Copyright © 2020 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControlller: UITabBarController {
+    
+}
